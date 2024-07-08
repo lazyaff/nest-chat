99fcdd47dfafdfd676533b82acea86380f6f771d
Faizal <faizalamri15@gmail.com>
2024-07-08 13:19:10 +0700

    feat(config): set up rabbit mq

---
+++ libs/common/src/rmq/remq.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { RmqOptions, Transport } from '@nestjs/microservices';
+
+@Injectable()
+export class RemqService {
+  constructor(private readonly configService: ConfigService) {}
+
+  getOptions(queue: string, noAck = false): RmqOptions {
+    return {
+      transport: Transport.RMQ,
+      options: {
+        urls: [this.configService.get<string>('RABBIT_MQ_URI')],
+        queue: this.configService.get<string>(
+          `RABBIT_MQ_${queue.toUpperCase()}_QUEUE`,
+        ),
+        noAck,
+        persistent: true,
+      },
+    };
+  }
+}
